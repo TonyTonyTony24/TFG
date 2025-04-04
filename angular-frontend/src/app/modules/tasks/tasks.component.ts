8681fbcd1c8e60876c3b0f997fee6328b30e75fb
Antonio Jesús <antonioyugos@gmail.com>
2025-04-04 09:38:55 +0200

    creamos primero el componente Toast y el servicio Toaster y también hemos configurado la interfaz toast

--- angular-frontend/src/app/modules/tasks/tasks.component.ts
+++ angular-frontend/src/app/modules/tasks/tasks.component.ts
@@ -3,76 +3,100 @@ import { CommonModule } from '@angular/common';
 import { TaskService } from '../../shared/services/task-service.service';
 import { Task } from '../../shared/interfaces/task';
 import { RouterModule } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { Subject, takeUntil, debounceTime, distinctUntilChanged } from 'rxjs';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+
+
+
 @Component({
   selector: 'app-tasks',
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss'],
+  styleUrls: ['./tasks.component.scss']
 })
-
-
 export class TasksComponent implements OnInit {
-  userTask!: FormGroup;
-  searchBar = new FormControl('');
   tasks: Task[] = [];
-  filteredTasks: Task[] = [];
-  private readonly destroy$ = new Subject <void>();
-  constructor(private taskService: TaskService, private fb: FormBuilder) {}
+  taskForm: FormGroup;
+  filteredTasks: Task [] = [];
+  searchControl: FormControl;
+
+
+  constructor(private taskService: TaskService) {
+
+    this.taskForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(3)]), 
+    })
+
+    this.searchControl = new FormControl ('');
+
+  }
+
   ngOnInit(): void {
     this.loadTasks();
-    this.searchSubscription();
-    this.userTask = this.fb.group({
-      id: ['0'],
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      completed: [false],
-    });
+
+    this.searchControl.valueChanges
+    .pipe(
+      debounceTime(100),
+      distinctUntilChanged()
+    ).subscribe(value => this.filterTasks(value))
   }
+
   loadTasks(): void {
-    this.taskService.getTasks().pipe(takeUntil(this.destroy$)).subscribe(
-      (tasks) => {
-        this.tasks = tasks;
-        this.filterTasks(this.searchBar.value || '');
-      },
+    this.taskService.getTasks().subscribe(
+      (tasks) => 
+        {this.tasks = tasks
+        this.filteredTasks = []; 
+      }, 
       (error) => console.error('Error fetching tasks:', error)
     );
   }
-  searchSubscription(): void {
-    this.searchBar.valueChanges.pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(userInput => {
-        this.filterTasks(userInput || '');
-      });
-  }
-  filterTasks(userInput: string): void {
-    if (!userInput) {
-      this.filteredTasks = [...this.tasks];
-      return;
+
+  addTask(): void {
+    if (this.taskForm.invalid) return;
+    const newTask: Task = { 
+      id: 0,
+       title: this.taskForm.value.title,
+       completed: false
+       
+       };
+
+    this.taskService.addTask(newTask).subscribe(() =>
+    {
+      this.loadTasks();
+      this.taskForm.reset();
+
+    });
+
     }
-    const lowerCase = userInput.toLowerCase();
-    this.filteredTasks = this.tasks.filter(task =>
-      task.title.toLowerCase().includes(lowerCase)
-    );
-  }
-  addTask(title: string): void {
-    if (this.userTask.valid) {
-      const newTask: Task = this.userTask.value;
-      this.taskService.addTask(newTask).subscribe(() => this.loadTasks());
-      this.userTask.reset();
-    }
-  }
-  get title() {
-    return this.userTask.get('title');
-  }
-  toggleTask(task: Task): void {
-    task.completed = !task.completed;
-    this.taskService.updateTask(task).subscribe(() => this.loadTasks());
+    toggleTask(task: Task): void {
+      task.completed = !task.completed;
+      this.taskService.updateTask(task).subscribe(() => this.loadTasks());
+     
   }
   deleteTask(id: number): void {
     this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
   }
+
+  filterTasks(searchTerm: string): void {
+    const term = searchTerm.toLowerCase();
+    
+    if (term === '') {
+      this.filteredTasks = [];
+      return;
+    }
+    
+    this.filteredTasks = this.tasks.filter(task =>
+      task.title.toLowerCase().includes(term)
+    )
+  }
+  
+
+  
+  
+
 }
