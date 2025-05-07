92b76d046be62f809187e242e5e6908027079ce8
Antonio Jesús <antonioyugos@gmail.com>
2025-05-07 14:57:56 +0200

    implementango pagina de registro y login

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './modules/tasks/tasks.component';
 import { LandingComponent } from './modules/landing/landing.component';
+import { AuthComponent } from './paginas/auth/auth.component';
 
 export const routes: Routes = [
     {path: "", component: LandingComponent},
-    {path: "tasks", component: TasksComponent}
+    {path: "tasks", component: TasksComponent},
+    {path: "auth", component: AuthComponent},
+
 ];
